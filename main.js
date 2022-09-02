66cabc42e7e623965148770a72477bd6df77ed06
Eric <eklee13@yahoo.com>
2022-09-01 18:44:59 -0700

    Made it look more like the apple calculator

---
+++ main.js
@@ -0,0 +1,7 @@
+
+
+let number = document.querySelectorAll('.number');
+let clear = document.querySelector('.clear');
+let equal = document.querySelector('.equal');
+let operator = document.querySelectorAll('.operator');
+let previous = document.querySelector('.previous');
